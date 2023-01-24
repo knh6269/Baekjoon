950997edc11c72431b4d757f87961b819ea693d7
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-01-24 21:20:57 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 1568 ms, Memory: 13432 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.js
@@ -0,0 +1,30 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+const solution = () => {
+
+    for (let i = 0; i < input; i++) {
+        for (let j = 0; j < i; j++) {
+            process.stdout.write(' ');
+        }
+
+        for (let k = 0; k < 2 * input - 2 * i - 1; k++) {
+            process.stdout.write('*');
+        }
+
+        console.log(' ');
+    }
+
+    for (let i = 2; i <= input; i ++) {
+        for (let j = 1; j <= input - i ; j++) {
+            process.stdout.write(' ');
+        }
+
+        for (let k = 0; k < 2 * i - 1; k++) {
+            process.stdout.write('*');
+        }
+        
+        console.log(' ');
+    }
+}
+
+solution();
