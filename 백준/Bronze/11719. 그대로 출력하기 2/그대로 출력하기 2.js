c1d00a76018cddc595f181b49873ed2ab6a78492
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-01-24 14:01:29 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 184 ms, Memory: 9492 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.js
@@ -0,0 +1,9 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+const solution = () => {
+    for (let i = 0; i < input.length; i++) {
+       console.log(input[i]);
+    }
+}
+
+solution();
