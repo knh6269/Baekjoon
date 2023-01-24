f7594a79f4667f4e40bb66e09e51a5d54055f2dd
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-01-24 16:54:01 +0900

    [Bronze V] Title: 합, Time: 124 ms, Memory: 9556 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.js
@@ -0,0 +1,13 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const solution = () => {
+    let sum = 0;
+
+    for (let i = 1; i <= input; i++) {
+        sum += i;
+    }
+
+    console.log(sum);
+}
+
+solution();
