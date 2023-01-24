fbb2daad17e2086c1ed17d4805c65e245c8207ca
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-01-24 17:25:54 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 696 ms, Memory: 13040 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.js
@@ -0,0 +1,17 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+const solution = () => {
+    for (let i = 0; i < input; i++) {
+
+        for (let k = 0; k < i ; k++) {
+            process.stdout.write(" ");
+        }
+
+        for (let j = 0; j < input - i; j++) {
+            process.stdout.write("*");
+        }
+        console.log('');
+    }
+}
+
+solution();
