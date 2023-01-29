3227116f1a70bd04a4f33707ab2fba759c9645a0
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-01-29 17:36:06 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 456 ms, Memory: 59404 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,26 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+input.splice(0, 1);
+
+const solution = () => {
+    input = input.map((item) => item.split(' '));
+
+    for (let i = 0; i < input.length; i++) {
+        for (let j = 0; j < 2; j++) {
+            input[i][j] = Number(input[i][j])
+        }
+    }
+    
+    input.sort((function comperator (a, b) {
+        if (a[0] > b[0]) return 1;
+        else if (a[0] < b[0]) return -1;
+        else {
+            if (a[1] > b[1]) return 1;
+            if (a[1] < b[1]) return -1;
+        }
+    }))
+    
+    input = input.map((item) => item.join(" "))
+    console.log(input.join('\n'));
+}
+
+solution();
