5bcb900992a13ea1daf933b18800fb6e98e0dd15
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-02-04 20:35:28 +0900

    [Gold IV] Title: 이분 그래프, Time: 1972 ms, Memory: 252648 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.js
@@ -0,0 +1,64 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const many = Number(input.shift());
+input = input.map((item) => item.split(' ').map(Number));
+
+for (let i = 0; i < many; i++) {
+    const [a, b] = input.shift().map(Number);
+    const edge = input.splice(0, b);
+    const graph = [...new Array(a + 1)].map(() => []);
+    let visited = [...new Array(a + 1)].map(() => 0);
+
+    edge.map(([to, from]) => {
+        graph[to].push(from);
+        graph[from].push(to);
+    });
+
+    const bfs  = (start) => {
+        let queue = [start];
+        let check = 1;
+
+        visited[start] = check;
+
+        while (queue.length) {
+            let cur = queue.shift();
+
+            if (visited[cur] === 1) {
+                check = 2;
+            } else if (visited[cur] === 2) {
+                check = 1;
+            }
+
+            for (let j = 0; j < graph[cur].length; j++) {
+                let next = graph[cur][j];
+
+                if (!visited[next]) {
+                    visited[next] = check;
+                    queue.push(next);
+                }
+                else if (visited[cur] === visited[next]) {
+                    return ;
+                } 
+            }
+        }
+    }
+
+    for (let j = 1; j <= a; j++) {
+        if (!visited[j])
+        bfs (j);
+    }
+
+    const isAns = () => {
+        for (let j = 1; j <= a; j++) {
+            for (let k = 0; k < graph[j].length; k++) {
+                let next = graph[j][k];
+    
+                if (visited[j] === visited[next]) {
+                    return console.log('NO');
+                }
+            }
+        }
+        return console.log('YES');
+    }
+    isAns();
+}
+
