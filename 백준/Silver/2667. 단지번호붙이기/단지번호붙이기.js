10df090e1673887de50b4bc5eacaec931664268e
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-02-07 23:29:35 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -1,31 +1,32 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 input.splice(0, 1);
-input = input.map((item) => item.split('').map(Number))
 let answer = [];
 let trys = 0;
+const visited= [...new Array(input.length + 1)].map(() => [...new Array(input.length + 1)].fill(0));
 
 const bfs = (queue, count) => {
     while (queue.length) {
         const [from, to] = queue.shift();
 
+        if (!visited[from][to]) {
+            visited[from][to] = 1;
             input[from][to] = 0;
             count++;
+        } else {
+            continue;
+        }
 
-        if (from < input.length - 1 && input[from + 1][to] == 1) {
+        if (from < input.length - 1 && input[from + 1][to] == 1 && !visited[from + 1][to]) {
             queue.push([from + 1, to]);
-            input[from + 1][to] = 0;
         }
-        if (to < input[0].length - 1 && input[from][to + 1] == 1 ) {
+        if (to < input[0].length - 1 && input[from][to + 1] == 1 && !visited[from][to + 1]) {
             queue.push([from, to + 1]);
-            input[from][to + 1] = 0;
         }
-        if (from > 0 && input[from - 1][to] == 1) {
+        if (from > 0 && input[from - 1][to] == 1 && !visited[from - 1][to]) {
             queue.push([from - 1, to]);
-            input[from - 1][to] = 0;
         }
-        if (to > 0 &&  input[from][to - 1] == 1) {
+        if (to > 0 &&  input[from][to - 1] == 1 && !visited[from][to - 1]) {
             queue.push([from, to - 1]);
-            input[from][to - 1] = 0;
         }
     }
     answer.push(count);
@@ -33,7 +34,7 @@ const bfs = (queue, count) => {
 
 for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < input[0].length; j++) {
-        if (input[i][j] == 1) {
+        if (input[i][j] == 1 && !visited[i][j]) {
             trys += 1;
             bfs([[i, j]], 0);
         }
