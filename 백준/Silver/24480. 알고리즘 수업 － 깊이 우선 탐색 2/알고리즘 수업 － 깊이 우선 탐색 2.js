1ee5613ddc09129ba9447a8ce1da9d6370b868ed
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-02-22 13:27:32 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 768 ms, Memory: 108772 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.js
@@ -0,0 +1,37 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const [a, b, c]= input.shift().split(' ').map(Number);
+
+input = input.map((item) => item.split(' ').map(Number));
+
+const graph = [...new Array(a + 1)].map(() => []);
+const answer = Array(a + 1).fill(0);
+
+input.map(([from, to]) => {
+    graph[from].push(to);
+    graph[to].push(from);
+})
+
+
+const visited = Array(a).fill(0);
+
+const dfs = (start) => {
+    const stack = [start];
+    let count = 0;
+
+    while (stack.length) {
+        const node = stack.pop();
+        
+        if (!visited[node]) {
+            visited[node] = 1;
+            count++;
+            answer[node] += count;
+            stack.push(...graph[node].sort((a, b) => a- b));
+        } else {
+            continue;
+        }
+    }
+}
+
+dfs(c);
+answer.shift();
+console.log(answer.join('\n'));
