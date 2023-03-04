a11631dd1590eb0bd3f93f3e38d01833044ac3cc
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-03-04 18:30:25 +0900

    [Silver III] Title: 두 수의 합, Time: 216 ms, Memory: 18672 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.js
@@ -0,0 +1,29 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+input.shift();
+
+const array = input[0].split(' ').map(Number).sort((a, b) => a - b);
+
+const target = Number(input[1]);
+
+let start = 0;
+let end = array.length - 1;
+let answer = 0;
+
+while (start < end) {
+    const sum = array[start] + array[end];
+
+    if (sum > target) {
+        end --;
+    }
+    if (sum < target) {
+        start ++;
+    }
+    if (sum === target) {
+        answer++;
+        start = start + 1;
+        end = end - 1;
+    }
+}
+
+console.log(answer)
