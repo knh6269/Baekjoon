e8488534da4a5ac055ba3ba9c207eeb691021764
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-03-15 22:56:00 +0900

    [unrated] Title: 콜라 문제, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -0,0 +1,10 @@
+function solution(a, b, n) {
+    let myCock = 0;
+    
+    while (n >= a) {
+        const newCock = b * Math.floor(n / a);
+        myCock += newCock;
+        n = n % a + newCock;
+    }
+    return myCock;
+}
