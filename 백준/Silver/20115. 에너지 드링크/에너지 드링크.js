b042ed43fbfb88e7626ac75a36ba27ff99c65cae
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-04-27 14:04:21 +0900

    [Silver III] Title: 에너지 드링크, Time: 268 ms, Memory: 24200 KB -BaekjoonHub

--- 백준/Silver/20115. 에너지 드링크/에너지 드링크.js
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.js
@@ -2,10 +2,6 @@ let [M, input] = require('fs').readFileSync('/dev/stdin').toString().trim().spli
 input = input.split(' ').map(Number).sort((a, b) => b - a)
 
 for (let i = 0; i < input.length - 1; i++) {
-    if (input[i] < input[i + 1]) {
-        input[i + 1] = input[i] / 2 + input[i + 1]
-        continue
-    } 
     input[i + 1] = input[i] + input[i + 1] / 2
 }
 
