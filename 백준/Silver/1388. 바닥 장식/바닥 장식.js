de150d4f3e28ffb7297715127d91833230e65c6b
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-05-16 22:56:24 +0900

    [Silver IV] Title: 바닥 장식, Time: 232 ms, Memory: 11248 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.js
@@ -0,0 +1,33 @@
+let [N, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const visited = [... new Array(input.length)].map(() => Array(input[0].length).fill(0));
+let count = 0;
+
+const bfs = (row, col) => {
+  const queue = [[row, col]];
+
+    while (queue.length) {
+      const [x,y] = queue.shift();
+      
+      if (visited[x][y]) continue;
+
+      visited[x][y] = 1;
+      if (y + 1 < input[0].length && input[x][y] === '-' && input[x][y + 1] === '-') {
+        queue.push([x, y+ 1]);
+      } 
+      if (x + 1 < input.length && input[x][y] === '|' && input[x + 1][y] === '|') {
+        queue.push([x + 1, y]);
+      } 
+    }
+}
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < input[0].length; j++) {
+    if (!visited[i][j]) {
+      bfs(i,j);
+      count++;
+    }
+  }
+}
+
+console.log(count);
