d1d6750b4de68f0d3c55a3df24952de40a68d748
knh6269 <44117975+knh6269@users.noreply.github.com>
2023-09-26 15:14:09 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,15 @@
+function solution(wallpaper) {
+    let [lux, luy, rdx, rdy] = [50,50,0,0];
+    
+    for (let i = 0; i < wallpaper.length; i++) {
+        for (let j = 0 ; j < wallpaper[i].length; j++) {
+            if (wallpaper[i][j] === '#') { 
+                lux = Math.min(lux, i);
+                luy = Math.min(luy, j);
+                rdx = Math.max(rdx, i);
+                rdy = Math.max(rdy, j);
+            }
+        }
+    }
+    return [lux, luy, rdx + 1, rdy + 1];
+}
