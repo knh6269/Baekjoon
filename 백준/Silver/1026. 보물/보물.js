b954d828ed16e9d383bb7908d5bea3d9e6a3f16d
knh6269 <44117975+knh6269@users.noreply.github.com>
2024-01-15 14:06:29 +0900

    [Silver IV] Title: 보물, Time: 120 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.js
+++ 백준/Silver/1026. 보물/보물.js
@@ -1,12 +1,18 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const N = Number(input.shift()); 
 
-const [a, b] = input.map((item) => item.split(' ').map(Number).sort((a,b) => a - b));
+const [a, b] = input.map((item) => item.split(' ').map(Number));
+
+const sortedA = a.sort((a, b) => a - b); 
 
 let sum = 0;
 
-for (let i = 0; i < N; i++) {
-    sum += a[i] * b[N - i - 1] ;   
-} 
+for (let num of sortedA) {
+    const maxValue = Math.max(...b);
+    const maxIndex = b.indexOf(maxValue);
+
+    sum += maxValue * num;
+    b.splice(maxIndex, 1);
+}
 
 console.log(sum);
