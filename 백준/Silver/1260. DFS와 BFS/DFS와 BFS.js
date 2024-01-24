ddcb6f23db90c5ec3038494b940155ba6390283a
knh6269 <44117975+knh6269@users.noreply.github.com>
2024-01-24 18:34:29 +0900

    [Silver II] Title: DFS와 BFS, Time: 260 ms, Memory: 16196 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -1,50 +1,53 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [N, M, V] =  input.shift().split(' ').map(Number);
-let edge = input.map((item) => item.split(' ').map(Number));
-let graph = [...new Array(N + 1)].map(() => []);
-
-edge.map(([a, b]) => {
-    graph[a].push(b);
-    graph[b].push(a);
+const [N, M, V] = input.shift().split(' ').map(Number);
+const map = input.map((item) => item.split(' ').map(Number));
+const graph = [... new Array(N + 1)].map(() => []);
+ 
+map.forEach(([from, to]) => {
+    graph[from].push(to);
+    graph[to].push(from);
 });
-
-
-const dfs = (start) => {
-    const stack = [start];
-    const order = [];
-    let visited = new Array(N + 1).fill(false);
+ 
+const dfs = (startIndex) => {
+    const visited = [...new Array(N + 1)].fill(0);
+    const stack = [startIndex];
+    const array = [];
 
     while (stack.length) {
-        const node = stack.pop(); 
+        const node = stack.pop();
 
-        if (!visited[node]) {
-            visited[node] = true;
-            order.push(node);
-            stack.push(...graph[node]);
-        }
+        if (visited[node]) continue;
+
+        array.push(node);
+        visited[node] = 1;
+
+        const newNode = graph[node].sort((a, b) => b - a);
+        stack.push(...newNode);
+
     }
-    console.log(order.join(' '));
+    console.log(array.join(' '))
 }
 
-const bfs = (start) => {
-    const queue = [start];
-    const order = [];
-    let visited = new Array(N + 1).fill(false);
+const bfs = (startIndex) => {
+    const visited = [...new Array(N + 1)].fill(0);
+    const queue = [startIndex];
+    const array = [];
 
     while (queue.length) {
         const node = queue.shift();
 
-        if (!visited[node]) {
-            visited[node] = true;
-            order.push(node);
-            queue.push(...graph[node]);
-        }
+        if (visited[node]) continue;
+        
+        array.push(node);
+        visited[node] = 1;
+
+        const newNode = graph[node].sort((a, b) => a - b);
+        queue.push(...newNode);
+
     }
-    console.log(order.join(' '));
+    console.log(array.join(' '))
 }
 
-graph.map((item) => item.sort((a, b) => b- a));
 dfs(V);
-graph.map((item) => item.sort((a, b) => a - b));
 bfs(V);
