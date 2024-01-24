502742c2035d5d9da9d65c6845c24b6f6d67e396
knh6269 <44117975+knh6269@users.noreply.github.com>
2024-01-24 16:01:22 +0900

    [Silver IV] Title: 바닥 장식, Time: 124 ms, Memory: 9396 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.js
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.js
@@ -1,4 +1,4 @@
-let  input  = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [N, M] = input.shift().split(' ').map(Number);
   
@@ -6,34 +6,38 @@ const [N, M] = input.shift().split(' ').map(Number);
 const visited = [...new Array(N)].map(() => new Array(M).fill(0));
 
 let answer = 0; 
- 
-const dfs = (row, col) => {
-    const stack = [[row, col]];
 
-    while (stack.length) {
-        const [nodeRow, nodeCol] = stack.pop();
+const dfs = (i, j) => { 
 
-        if (visited[nodeRow][nodeCol]) continue;
+    if (input[i][j] === '-') {
+        for (let k = j; k < M; k++) {
+            if (input[i][k] === '-') {
+                visited[i][k] = 1;
+            } else {
+                break;
+            }
+        }
+    }
 
-        visited[nodeRow][nodeCol] = 1;
- 
-             if (nodeCol + 1 < M && input[nodeRow][nodeCol] === '-' && input[nodeRow][nodeCol + 1] === '-') { 
-                stack.push([nodeRow, nodeCol + 1]);
-             } 
- 
-            if (nodeRow + 1 < N && input[nodeRow][nodeCol] === '|' && input[nodeRow + 1][nodeCol] === '|') {
-                stack.push([nodeRow + 1, nodeCol]);
-            } 
+    if (input[i][j] === '|') {
+        for (let k = i; k < N; k++) {
+            if (input[k][j] === '|') {
+                visited[k][j] = 1;
+            } else {
+                break;
+            }
+        }
     }
 }
 
-for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input[0].length; j++) {
-      if (!visited[i][j]) {
-        dfs(i,j);
-        answer++;
-      }
+for (let i = 0 ; i < N; i++) {
+    for (let j = 0; j < M; j++) {  
+        if (!visited[i][j]) {
+            visited[i][j] = 1;
+            dfs(i, j);
+            answer++; 
+        }
     }
-  }
-  
+}
+
 console.log(answer);
